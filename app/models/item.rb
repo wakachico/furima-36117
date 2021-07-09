25fc3caece83a7df388607eb655370d85e6b124c
wakachico <86215584+wakachico@users.noreply.github.com>
2021-07-09 17:28:07 +0900

    new、createアクションの追加、微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_time
-  has_one_attached :image
+  
 
   validates :name,         presence: true
   validates :text,         presence: true
-  validates :selling_price,presence: true
+  validates :selling_price,presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :shipping_cost_id, numericality: { other_than: 1 }
-  validates :prefencture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_time_id, numericality: { other_than: 1 }
   
 end
