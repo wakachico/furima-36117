146dbcea4320d2d526cd0a675a026aacb58a3288
wakachico <86215584+wakachico@users.noreply.github.com>
2021-07-11 16:48:09 +0900

    ordersTableのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
