5a943bccd482e28869d3e082c1902e9fd26b942c
wakachico <86215584+wakachico@users.noreply.github.com>
2021-07-11 17:15:59 +0900

    addressesTableのアソシエーション、バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,13 @@
 class Address < ApplicationRecord
+  belongs_to :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :city, :block
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を「3桁の数値-4桁の数値」の形で入力してください' }
+    validates :prefecture, numericality: { other_than: 1 }
+    validates :phone_number,  format: { with: /\A\d{10,11}\z/, message: 'を10桁or11桁の数値で入力してください' }
+  end
+
 end
