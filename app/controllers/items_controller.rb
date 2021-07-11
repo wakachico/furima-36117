95bd75fc20bfb4ee9e3e1ddefd837a7994ae5f99
wakachico <86215584+wakachico@users.noreply.github.com>
2021-07-11 16:23:56 +0900

    レビュー修正（セキュリティ強化）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :destory]
+  before_action :move_to_index, only: [:edit, :update, :destory]
 
 
   def index
