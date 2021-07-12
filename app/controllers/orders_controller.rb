0ee083aa12f0c70b725c2b4e915a57ac889990a1
wakachico <86215584+wakachico@users.noreply.github.com>
2021-07-12 18:38:50 +0900

    カード情報の取得

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,18 +7,29 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
 
+  def pay_item
+    Payjp.api_key = "sk_test_c15578ac9893fb3c223c2649"
+    Payjp::Charge.create(
+        amount: Item.find(order_params[:item_id]).selling_price,
+        card: order_params[:token],
+        currency: 'jpy'
+    )
+  end
+
   def order_params
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :block, :building, :phone_number
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :token, :item_id, :user_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number
 
   with_options presence: true do
-    validates :item_id, :user_id, :city, :block
+    validates :item_id, :user_id, :city, :block, :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を「3桁の数値-4桁の数値」の形で入力してください' }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :phone_number,  format: { with: /\A\d{10,11}\z/, message: 'を10桁or11桁の数値で入力してください' }
